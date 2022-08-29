778c52a8f004bc0cb4b2f90916c09ca9a97e03e6
ubed-ali <syedubed09@gmail.com>
2022-08-29 11:24:02 +0530

    UA | MailController

---
+++ src/main/kotlin/sendgridEmail/Application.kt
@@ -0,0 +1,14 @@
+package sendgridEmail
+
+import io.micronaut.runtime.Micronaut.build
+import java.lang.Thread.sleep
+
+fun main(args: Array<String>)  {
+			 println("Entering into '${Thread.currentThread().name}' thread") //1
+				build()
+					.args(*args)
+					.packages("com.sendgridEmail")
+					.start()
+		sleep(10000) //pretend we are doing something useful here
+		println("Exiting from '${Thread.currentThread().name}' thread")
+}
---
+++ src/main/kotlin/sendgridEmail/MailController.kt
@@ -0,0 +1,63 @@
+package sendgridEmail
+
+import com.sendgrid.Request
+import com.sendgrid.Response
+import io.micronaut.email.AsyncEmailSender
+import io.micronaut.email.BodyType
+import io.micronaut.email.Email
+import io.micronaut.http.*
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
+import kotlinx.coroutines.coroutineScope
+import org.reactivestreams.Publisher
+import org.slf4j.LoggerFactory
+import reactor.core.publisher.Mono
+import java.lang.Thread.sleep
+
+
+@Controller("/mail")
+class MailController(private val emailSender: AsyncEmailSender<Request, Response>) {
+
+  @Post("/send/1")
+  suspend fun sendEmail1(@Body("to") to: String): Publisher<HttpResponse<*>> = coroutineScope {
+    sleep(7000) //pretend we are doing something useful here
+    println("Sending e-mail...")
+    println("POST /send/1 is working on '${Thread.currentThread().name}' thread")
+    return@coroutineScope Mono.from(
+      emailSender.sendAsync(
+        Email.builder()
+          .to(to)
+          .subject("Sending e-mail from 1st POST method using '${Thread.currentThread().name}' thread")
+          .body("Hello <em>${to},</em><br>" +
+            "Thank you for subscribing to our <strong>weekly blogs</strong>", BodyType.HTML)
+      )
+    ).map { rsp: Response ->
+        if (rsp.statusCode >= 400)
+          HttpResponse.unprocessableEntity()
+        else
+          HttpResponse.accepted<Any>()
+    }
+  }
+
+  @Post("/send/2")
+  suspend fun sendEmail2(@Body("to") to: String): Publisher<HttpResponse<*>> = coroutineScope {
+    sleep(1000) //pretend we are doing something useful here too
+    println("Sending e-mail...")
+    println("POST /send/2 is working on '${Thread.currentThread().name}' thread")
+    return@coroutineScope Mono.from(
+      emailSender.sendAsync(
+        Email.builder()
+          .to(to)
+          .subject("Sending e-mail from 2nd POST method using '${Thread.currentThread().name}' thread")
+          .body("Hello <em>${to},</em><br>" +
+            "Thank you for subscribing to our <strong>weekly blogs</strong>", BodyType.HTML)
+      )
+    ).map{ rsp: Response ->
+        if (rsp.statusCode >= 400)
+          HttpResponse.unprocessableEntity()
+        else
+          HttpResponse.accepted<Any>()
+      }
+  }
+}
